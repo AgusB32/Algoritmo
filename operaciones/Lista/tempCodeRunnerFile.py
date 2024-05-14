3c502fd5144a52d57cc6b5686260a9b725c8bc58
AgusB32 <fr.abenedetti1@gmail.com>
2024-05-14 18:06:01 -0300

    lista

--- operaciones/Lista/tempCodeRunnerFile.py
+++ operaciones/Lista/tempCodeRunnerFile.py
@@ -1,17 +1,35 @@
-numeros = [22, 11, 30, 7, 13, 1]
-print("Lista original", numeros)
-def primo(numero):
-    if numero <=1:
-        return False
-    if numero <=3:
-        return True
-    if numero % 2 == 0 or numero % 3 == 0:
-        return False
-    i = 5
-    while i * i <= numero:
-        if numero % i == 0 or numero % (i+2) == 0:
-            return False
-        i += 6
-    return True
-sin_primos = [numero for numero in numeros if not primo(numero)]
-print("Lista sin primos", sin_primos)
+from lista import search
+
+super_heroes = [
+    {"nombre": "Linterna Verde", "año_aparicion": 1940, "casa_comic": "DC Comics", "biografia": "Miembro de la Tropa de Linternas Verdes, posee un anillo que le otorga poderes basados en la fuerza de voluntad."},
+    {"nombre": "Wolverine","año_aparicion": 1974,"casa_comic": "Marvel Comics","biografia": "Mutante con garras retráctiles y habilidades regenerativas, miembro de los X-Men."},
+    {"nombre": "Doctor Strange","año_aparicion": 1963,"casa_comic": "Marvel Comics","biografia": "Hechicero supremo del universo Marvel, maestro de las artes místicas y protector de la realidad."},
+    {"nombre": "Capitana Marvel","año_aparicion": 1968,"casa_comic": "Marvel Comics","biografia": "Heroína cósmica con poderes de vuelo, fuerza sobrehumana y energía cósmica."},
+    {"nombre": "Mujer Maravilla","año_aparicion": 1941,"casa_comic": "DC Comics","biografia": "Princesa amazona y una de las principales defensoras de la justicia y la igualdad en el Universo DC."},
+    {"nombre": "Flash","año_aparicion": 1940,"casa_comic": "DC Comics","biografia": "Velocista con la capacidad de correr a velocidades superiores a la luz, miembro de la Liga de la Justicia."},
+    {"nombre": "Star-Lord","año_aparicion": 1976,"casa_comic": "Marvel Comics","biografia": "Líder de los Guardianes de la Galaxia, experto en combate y estrategia intergaláctica."},
+    {"nombre": "Superman","año_aparicion": 1938,"casa_comic": "DC Comics","biografia": "El Hombre de Acero, uno de los héroes más icónicos de DC con superpoderes sobrehumanos."},
+    {"nombre": "Batman","año_aparicion": 1939,"casa_comic": "DC Comics","biografia": "El Caballero Oscuro, detective y luchador experto que protege Gotham City."},
+    {"nombre": "Iron Man","año_aparicion": 1963,"casa_comic": "Marvel Comics","biografia": "Tony Stark, genio multimillonario y superhéroe con una armadura tecnológica de alta tecnología."},
+    {"nombre": "Wonder Woman","año_aparicion": 1941,"casa_comic": "DC Comics","biografia": "La princesa amazona Diana, guerrera y defensora de la paz y la justicia en el mundo."},
+    {"nombre": "Spider-Man","año_aparicion": 1962,"casa_comic": "Marvel Comics","biografia": "Peter Parker, joven héroe con habilidades arácnidas tras ser picado por una araña radiactiva."},
+    {"nombre": "Thor","año_aparicion": 1962,"casa_comic": "Marvel Comics","biografia": "Dios nórdico del trueno y miembro de los Vengadores, posee un martillo encantado llamado Mjolnir."},
+    {"nombre": "Aquaman","año_aparicion": 1941,"casa_comic": "DC Comics","biografia": "Rey de Atlantis con la capacidad de comunicarse con la vida marina y controlar el agua."},
+    {"nombre": "Green Arrow","año_aparicion": 1941,"casa_comic": "DC Comics","biografia": "Oliver Queen, arquero habilidoso y defensor de la justicia con su arco y flechas."},
+    {"nombre": "Hulk","año_aparicion": 1962,"casa_comic": "Marvel Comics","biografia": "Bruce Banner, científico transformado en monstruo verde con fuerza increíble."},
+    {"nombre": "Black Widow","año_aparicion": 1964,"casa_comic": "Marvel Comics","biografia": "Natasha Romanoff, espía rusa y experta en combate mano a mano y armas."},
+    {"nombre": "Mr. Fantástico","año_aparicion": 1961,"casa_comic": "Marvel Comics","biografia": "Líder de los 4 Fantásticos, científico brillante con la capacidad de estirar y deformar su cuerpo."},
+    {"nombre": "La Mujer Invisible","año_aparicion": 1961,"casa_comic": "Marvel Comics","biografia": "Miembro de los 4 Fantásticos, posee el poder de hacerse invisible y crear campos de fuerza."},
+    {"nombre": "La Antorcha Humana","año_aparicion": 1961,"casa_comic": "Marvel Comics","biografia": "Miembro de los 4 Fantásticos, puede envolverse en llamas y volar a altas velocidades."},
+    {"nombre": "La Cosa","año_aparicion": 1961,"casa_comic": "Marvel Comics","biografia": "Miembro de los 4 Fantásticos, posee una fuerza y resistencia sobrehumanas, con piel rocosa."},
+    {"nombre": "Capitán América","año_aparicion": 1941,"casa_comic": "Marvel Comics","biografia": "El supersoldado Steve Rogers, símbolo de libertad y justicia con escudo indestructible."},
+    {"nombre": "Ant-Man","año_aparicion": 1962,"casa_comic": "Marvel Comics","biografia": "Hank Pym o Scott Lang, héroes capaces de cambiar de tamaño y comunicarse con insectos."}
+]
+
+index = search(super_heroes,"nombre","Linterna Verde")
+
+if index is not None:
+    super_hero = super_heroes[index]
+    print (f"El año de aparicion de {super_hero["nombre"]} es {super_hero["año_aparicion"]}")
+else:
+    primo("No esta en la lista")
--- operaciones/Pila_Cola/Pila_ejercicios.py
+++ operaciones/Pila_Cola/Pila_ejercicios.py
@@ -1,7 +1,7 @@
-# Se tienen dos pilas con personajes de Star Wars, en una los del episodio V de 
-# “The empire strikes back” y la otra los del episodio VII “The force awakens”. Desarrollar un 
-# algoritmo que permita obtener la intersección de ambas pilas, es decir los personajes
-# que aparecen en ambos episodios.
+# 16) Se tienen dos pilas con personajes de Star Wars, en una los del episodio V de 
+#     “The empire strikes back” y la otra los del episodio VII “The force awakens”. Desarrollar un 
+#     algoritmo que permita obtener la intersección de ambas pilas, es decir los personajes
+#     que aparecen en ambos episodios.
 
 from pila import Stack
 
@@ -15,3 +15,22 @@ while epV.size > 0:
 
 epV = ['Lobot','Han Solo','Bossk','Dengar','Chewbacca']
 epVII = ['Kylo Ren','Luke Skywalker', 'Han Solo','Chewbacca','BB-8']
+
+
+
+
+
+
+# 24) Dada una pila de personajes de Marvel Cinematic Universe (MCU), de los cuales se dispone de
+#     su nombre y la cantidad de películas de la saga en la que participó, implementar las funciones
+#     necesarias para resolver las siguientes actividades:
+
+# a. determinar en qué posición se encuentran Rocket Raccoon y Groot, tomando como posición 
+# uno la cima de la pila.
+
+# b. determinar los personajes que participaron en más de 5 películas de la saga, además indicar
+# la cantidad de películas en la que aparece.
+
+# c. determinar en cuantas películas participo la Viuda Negra (Black Widow).
+
+# d. mostrar todos los personajes cuyos nombre empiezan con C, D y G.
